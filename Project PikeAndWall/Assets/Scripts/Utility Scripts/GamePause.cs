a25d301834554630c04c5eef22c50d7aa5d86f3a
Fabio <fmaier67@gmail.com>
2022-06-13 19:04:55 +0200

    Interruptgame

---
+++ Project PikeAndWall/Assets/Scripts/Utility Scripts/GamePause.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isInterrupted = false;
+    public GameObject pauseMenu;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)&&!isInterrupted)
+        {
+            //Debug.Log("Escape!");
+            pauseMenu.SetActive(true);
+            InterruptGame();
+            
+        }
+        if(Input.GetKeyDown(KeyCode.Escape) && isInterrupted)
+        {
+            pauseMenu.SetActive(false);
+            ResumeGame();
+        }
+    }
+
+    // Update is called once per frame
+    public void InterruptGame()
+    {
+        isInterrupted = true;
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        isInterrupted = false;
+        Time.timeScale = 1;
+    }
+}
